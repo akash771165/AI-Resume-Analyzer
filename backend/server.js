1dade5d01880177585e33e8d62aae211059faca9
Akash Parmar <akashparmar77116@gmail.com>
2026-07-05 11:49:41 +0530

    Fix MongoDB environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
 connectDB();
+
 const uploadRoute = require("./routes/uploadRoute");
 const resumeRoute = require("./routes/resumeRoute");
 const authRoute = require("./routes/authRoute");
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/upload", uploadRoute);
 app.use("/api/resumes", resumeRoute);
 app.use("/api/auth", authRoute);
@@ -18,15 +21,16 @@ app.use("/api/auth", authRoute);
 app.get("/", (req, res) => {
     res.send("🚀 AI Resume Analyzer Backend is Running!");
 });
+
 app.get("/api/test", (req, res) => {
     res.json({
         success: true,
-        message: "Backend Connected Successfully 🚀"
+        message: "Backend Connected Successfully 🚀",
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
